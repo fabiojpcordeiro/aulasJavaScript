ac7b2b24a7c30afafea33896b02c2b10a9c76549
Fabio Cordeiro <fabiojp1997@hotmail.com>
2024-10-29 20:17:32 -0300

    Aula sobre APIs

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-let rotationDegree = 0;
-
-function rotateElement(){
-    rotationDegree += 45;
-    document.getElementById("rotating-div").style
-    .transform = `rotate(${rotationDegree}deg)`;    
+let arrayPokemons;
+function getPokemon(){
+    //fetch cham uma API
+    fetch("https://pokeapi.co/api/v2/pokemon/",
+        {
+            method: "GET"
+        }
+    )
+    .then ((response) => response.json())
+    .then ((data) => {
+        arrayPokemons = data.results;
+        appendData(arrayPokemons);
+    }
+    )
+    .catch((error) =>{
+        console.error ("erro", error);
+    }
+    ) 
 }
+function appendData (pokemons){
+    let place = document.querySelector("#data-output");
+    let output = "";
 
-function toggleFade(){
-    const div= document.getElementById("fading-div");
-    div.classList.toggle('visible');
+    for(let pokemon of pokemons){
+        output += `
+        <tr>
+        <td>${pokemon.name}</td>
+        <td>${pokemon.url}</td>
+        </td>
+        `
+    }
+    place.innerHTML = output;
 }
-
-function togglePulse(){
-    const div = document.getElementById('pulsing-div');
-    div.classList.toggle("pulsing");
-}
-
-function zoomin(){
-    const div= document.getElementById("zooming-div");
-    div.classList.add('zooming');
-}
+getPokemon();
